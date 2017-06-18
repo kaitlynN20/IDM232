20e216e2ea7655dd18bc1befb39b25c0cc24d400
Kaitlyn <kaitlynn281@gmail.com>
2017-06-17 23:31:36 -0400

    Add files via upload
    mysql database
    .php file

---
+++ Videos.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `kaitlyq7_IDM232_final`
+//
+
+// `kaitlyq7_IDM232_final`.`Videos`
+$Videos = array(
+  array('ID' => '1','img-source' => 'img/video_medium.jpg','vid-source' => 'https://www.youtube.com/embed/rO3V9YL5YB0?&theme=dark&color=white&autohide=2&modestbranding=1&showinfo=0&rel=0"frameborder="0','title' => 'Sweet Angel','caption' => 'by Timothy Bloom','description' => 'A music video I star in. The artist asked me to hang out and walk around New York. It is a lyric video for his grammy award winning song. The single is available for purchase on itunes.
+'),
+  array('ID' => '2','img-source' => 'img/video02_medium.jpg','vid-source' => 'https://www.youtube.com/embed/3-CJ9_I3wEE?&theme=dark&color=white&autohide=2&modestbranding=1&showinfo=0&rel=0"frameborder="0','title' => 'Somebody Told Me','caption' => 'by Das','description' => 'A music video I was asked to star in. It is about the artist following me around New York because he knows I’m cheating on him. You can purchases the single on itunes.'),
+  array('ID' => '3','img-source' => 'img/video03_medium.jpg','vid-source' => 'https://www.youtube.com/embed/P8v7C1YtpAA?&theme=dark&color=white&autohide=2&modestbranding=1&showinfo=0&rel=0"frameborder="0','title' => 'Never Gon’ Change','caption' => 'by The Noize','description' => 'I was asked to dance on stage with the artist during his live performance. ')
+);
